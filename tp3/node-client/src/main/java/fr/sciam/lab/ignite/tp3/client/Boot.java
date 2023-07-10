25abd15477165f5228edea48e019de7c0f1d5f95
Antoine Sabot-Durand <antoine@sabot-durand.net>
2023-07-10 10:59:48 +0200

    Add boot code in TP3

--- tp3/node-client/src/main/java/fr/sciam/lab/ignite/tp3/client/Boot.java
+++ tp3/node-client/src/main/java/fr/sciam/lab/ignite/tp3/client/Boot.java
@@ -1,6 +1,24 @@
 package fr.sciam.lab.ignite.tp3.client;
 
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteAtomicLong;
 
+@ApplicationScoped
+@RequiredArgsConstructor
+@Slf4j
 public class Boot {
 
+  public static final String RANDOM_INTEGERS = "randomIntegers";
+  private final Ignite ignite;
+  public void onBoot(@Observes StartupEvent startupEvent) {
+    log.info("Boot");
+    IgniteAtomicLong counter = ignite.atomicLong("counter", 0L, true);
+    counter.incrementAndGet();
+    log.info("==>Server {} started",counter.get());
+  }
 }
--- tp3/node-server/src/main/java/fr/sciam/lab/ignite/tp3/server/Boot.java
+++ tp3/node-server/src/main/java/fr/sciam/lab/ignite/tp3/server/Boot.java
@@ -1,5 +1,47 @@
 package fr.sciam.lab.ignite.tp3.server;
 
+import io.quarkus.runtime.Startup;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.cluster.ClusterGroup;
+import org.apache.ignite.services.ServiceConfiguration;
+
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@ApplicationScoped
+@RequiredArgsConstructor
+@Slf4j
 public class Boot {
 
+  public static final String RANDOM_INTEGERS = "randomIntegers";
+  private final Ignite ignite;
+
+  public void onBoot(@Observes StartupEvent startupEvent) {
+    log.info("Boot, create a Map of Random Integers");
+    IgniteCache<UUID, Integer> randomIntegers = ignite.getOrCreateCache(RANDOM_INTEGERS);
+    Random random = new Random();
+    AtomicInteger sum=new AtomicInteger(0);
+    for ( int index=0 ; index < 1000 ; index++) {
+      int val = random.nextInt(500);
+      sum.addAndGet(val);
+      randomIntegers.put(UUID.randomUUID(), val);
+    }
+    log.info("Sum is equal to {}",sum.get());
+
+    ClusterGroup grp = ignite.cluster().forAttribute("nodeType", "server");
+    log.info("grp {}",grp.nodes());
+    ignite.services(grp).deploy(
+        new ServiceConfiguration()
+            .setName("MySimpleService")
+            .setMaxPerNodeCount(1)
+            .setService(new QuarkusIgniteServiceProxy())
+    );
+  }
 }
