f90ca385ce7b1554d8de91ca4a615595954e80c7
Antoine Sabot-Durand <antoine@sabot-durand.net>
2023-07-10 11:42:40 +0200

    Revert TP1 solution

--- tp1-2/src/main/java/fr/sciam/lab/ignite/ConnectToIgniteCluster.java
+++ tp1-2/src/main/java/fr/sciam/lab/ignite/ConnectToIgniteCluster.java
@@ -1,29 +1,7 @@
 package fr.sciam.lab.ignite;
 
-import jakarta.enterprise.inject.Disposes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.ignite.client.ClientAtomicLong;
-import org.apache.ignite.client.IgniteClient;
 
-@Produces(MediaType.APPLICATION_JSON)
-@Path("/ignite/testconnect")
-@Slf4j
-@RequiredArgsConstructor
 public class ConnectToIgniteCluster {
-  private final IgniteClient igniteClient;
 
-  @GET
-  public Response testConnect() {
-    ClientAtomicLong hitCounter = igniteClient.atomicLong("hitCounter", 0L, true);
-    long counter = hitCounter.incrementAndGet();
-    log.info("Current hit {}", counter);
-    return Response.ok("{ Hit : \"" + counter + "\"}").build();
-  }
 
 }
--- tp1-2/src/main/java/fr/sciam/lab/ignite/IgniteClientProducer.java
+++ tp1-2/src/main/java/fr/sciam/lab/ignite/IgniteClientProducer.java
@@ -1,27 +1,6 @@
 package fr.sciam.lab.ignite;
 
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Disposes;
-import jakarta.enterprise.inject.Produces;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.ignite.Ignition;
-import org.apache.ignite.client.IgniteClient;
-import org.apache.ignite.configuration.ClientConfiguration;
 
-@ApplicationScoped
-@RequiredArgsConstructor
-@Slf4j
 public class IgniteClientProducer {
 
-  @Produces
-  @ApplicationScoped
-  public IgniteClient igniteClient() {
-    ClientConfiguration cfg = new ClientConfiguration().setAddresses("127.0.0.1:10800");
-    return Ignition.startClient(cfg);
-  }
-
-  public void disposeIgniteClient(@Disposes IgniteClient igniteClient) {
-    igniteClient.close();
-  }
 }
