4d9dc5cbd9ce3500d5a7151c2585da66b39785ef
yblazart <yann.blazart@bycode.fr>
2023-07-10 10:52:40 +0200

    Rollback removed code

--- tp3/node-client/src/main/java/fr/sciam/lab/ignite/tp3/client/stillusefull/CharacterCountTask.java
+++
@@ -1,7 +0,0 @@
-package fr.sciam.lab.ignite.tp3.client.stillusefull;
-
-import lombok.extern.slf4j.Slf4j;i
-@Slf4j
-public class CharacterCountTask extends ComputeTaskSplitAdapter<String, Integer> {
-
-}
