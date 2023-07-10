4c1bcbd6eb6405187019e3197632d9dccc3ae9d8
pedroelmoucho <pedroelmoucho@gmail.com>
2023-07-10 11:02:28 +0200

    Fix ignite producers

--- tp3/node-client/src/main/java/fr/sciam/lab/ignite/tp3/client/config/IgniteProducer.java
+++ tp3/node-client/src/main/java/fr/sciam/lab/ignite/tp3/client/config/IgniteProducer.java
@@ -1,22 +1,51 @@
 package fr.sciam.lab.ignite.tp3.client.config;
 
 
+import io.quarkus.runtime.configuration.ProfileManager;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Disposes;
 import jakarta.enterprise.inject.Produces;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
-import org.apache.ignite.client.IgniteClient;
-import org.apache.ignite.configuration.ClientConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import java.io.File;
+import java.util.Map;
+
+@Slf4j
+@ApplicationScoped
+@RequiredArgsConstructor
 public class IgniteProducer {
+    @ConfigProperty(name = "quarkus.http.port")
+    Integer httpPort;
+
     @Produces
     @ApplicationScoped
-    public IgniteClient igniteClient() {
-        ClientConfiguration cfg = new ClientConfiguration().setAddresses("127.0.0.1:10800");
-        return Ignition.startClient(cfg);
+    public Ignite ignite(IgniteConfiguration igniteConfiguration) {
+        return Ignition.start(igniteConfiguration);
     }
 
-    public void disposeIgniteClient(@Disposes IgniteClient igniteClient) {
-        igniteClient.close();
+    @Produces
+    @ApplicationScoped
+    public IgniteConfiguration igniteConfiguration() {
+        IgniteConfiguration igniteConfiguration = new IgniteConfiguration();
+        igniteConfiguration.setUserAttributes(Map.of("nodeType", "client"));
+        igniteConfiguration.setClientMode(true);
+        igniteConfiguration.setUserAttributes(Map.of("httpPort", httpPort.toString()));
+        igniteConfiguration.setClassLoader(Thread.currentThread().getContextClassLoader());
+        igniteConfiguration.setPeerClassLoadingEnabled(true);
+        if (ProfileManager.getLaunchMode().isDevOrTest()) {
+            String workDirPath = new File(".", "target/ignite").getAbsolutePath();
+            log.info("Ignite working dir set to {}", workDirPath);
+            igniteConfiguration.setWorkDirectory(workDirPath);
+        }
+        return igniteConfiguration;
+    }
+
+    public void disposeIgniteClient(@Disposes Ignite ignite) {
+        ignite.close();
     }
 }
