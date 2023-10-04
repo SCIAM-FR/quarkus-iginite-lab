5661bd1c472900886b1ab74ae5d4b6ee6f8a1c7f
Yann Blazart <yann.blazart@bycode.fr>
2023-10-04 11:01:39 +0200

    Update create-schema-operations.sql
    rework packages

--- tp1-2/src/main/resources/create-schema-operations.sql
+++ tp1-2/src/main/resources/create-schema-operations.sql
@@ -4,4 +4,4 @@ create table if not exists OPERATIONS
     accountId varchar(40),
     amount decimal(10,2),
     primary key (id, accountId)
-) with "TEMPLATE=PARTITIONED,BACKUPS=2,AFFINITY_KEY=accountId,CACHE_NAME=operations,VALUE_TYPE=fr.sciam.rivieradev2023.quarkusignite.tp1.model.Operation,KEY_TYPE=fr.sciam.rivieradev2023.quarkusignite.tp1.model.OperationKey";
+) with "TEMPLATE=PARTITIONED,BACKUPS=2,AFFINITY_KEY=accountId,CACHE_NAME=operations,VALUE_TYPE=fr.sciam.lab.ignite.model.Operation,KEY_TYPE=fr.sciam.lab.ignite.model.OperationKey";
