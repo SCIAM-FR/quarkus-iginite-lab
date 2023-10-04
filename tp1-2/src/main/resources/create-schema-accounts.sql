0867ae04f9c73b72a6ad26420bdd89fd7db7e1b0
Yann Blazart <yann.blazart@bycode.fr>
2023-10-04 10:59:55 +0200

    Update create-schema-accounts.sql
    Rework packages name

--- tp1-2/src/main/resources/create-schema-accounts.sql
+++ tp1-2/src/main/resources/create-schema-accounts.sql
@@ -3,7 +3,7 @@ create table if not exists ACCOUNTS
 (
     id varchar(40) primary key,
     amount decimal(10,2)
-) with "TEMPLATE=REPLICATED,BACKUPS=2,AFFINITY_KEY=id,CACHE_NAME=accounts,VALUE_TYPE=fr.sciam.rivieradev2023.quarkusignite.tp1.model.Account,KEY_TYPE=fr.sciam.rivieradev2023.quarkusignite.tp1.model.AccountKey";
+) with "TEMPLATE=REPLICATED,BACKUPS=2,AFFINITY_KEY=id,CACHE_NAME=accounts,VALUE_TYPE=fr.sciam.lab.ignite.model.Account,KEY_TYPE=fr.sciam.lab.ignite.model.AccountKey";
 
 
 
